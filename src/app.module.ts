9a8c6cdf47b9d78a07b7d3463b34de3763317bb5
Ramazan <badc0dead@outlook.com>
2020-06-27 01:09:39 +0200

    It's running!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import config from 'config';
+
+const dbConfig = config.get('postgres');
 
 @Module({
-  imports: [],
+  imports: [
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-54-247-78-30.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'kvdnxsyhjmsltd',
+      password: '18d841508e8bfab5593d07d81feef8fb0367825511d79bf79d9f1dfefcaf4154',
+      database: 'das3a18aek5aji',
+      entities: [],
+      synchronize: true,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
