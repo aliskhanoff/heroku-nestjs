39a0b51af6ecf7754afd173b44c11112ac3e14ac
Ramazan <badc0dead@outlook.com>
2020-06-26 21:41:55 +0200

    feat: Initial commit
    Initial commit with nestjs workspaces

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  
+  app.use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
